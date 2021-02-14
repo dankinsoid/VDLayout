8138f9b484a47a0770fa0ef6d828f861db122de5
Danil <voidilov@gmail.com>
2021-02-14 03:42:34 +0300

    1.0.10

---
+++ Sources/VDLayout/Chaining++.swift
@@ -0,0 +1,42 @@
+//
+//  File.swift
+//  
+//
+//  Created by Данил Войдилов on 13.02.2021.
+//
+
+import UIKit
+import VDKit
+import ConstraintsOperators
+
+extension ValueChaining: UILayoutableArray where W: UILayoutableArray {
+	
+	public func asLayoutableArray() -> [UILayoutable] {
+		wrappedValue.asLayoutableArray()
+	}
+	
+}
+
+extension ValueChaining: UILayoutable where W: SubviewProtocol {
+	public var itemForConstraint: Any {
+		wrappedValue.itemForConstraint
+	}
+}
+
+extension ValueChaining: SubviewsArrayConvertable where W: SubviewProtocol {
+	public func asSubviews() -> [SubviewProtocol] {
+		wrappedValue.asSubviews()
+	}
+}
+
+extension ValueChaining: SubviewProtocol where W: SubviewProtocol {
+	
+	public func viewToAdd() -> UIView {
+		wrappedValue.viewToAdd()
+	}
+	
+	public func didAdded(to superview: UIView) {
+		apply().didAdded(to: superview)
+	}
+	
+}
