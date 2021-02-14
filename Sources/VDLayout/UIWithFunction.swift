4dc6cda025367ae6ec307dbf332eadc4bd28033f
Danil <voidilov@gmail.com>
2021-02-14 23:44:28 +0300

    1.8.0

---
+++ Sources/VDLayout/UIWithFunction.swift
@@ -0,0 +1,41 @@
+//
+//  File.swift
+//  
+//
+//  Created by Данил Войдилов on 14.02.2021.
+//
+
+import UIKit
+
+open class LtView: UIView {
+	
+	public override init(frame: CGRect) {
+		super.init(frame: frame)
+		afterInit()
+	}
+	
+	public required init?(coder: NSCoder) {
+		super.init(coder: coder)
+		afterInit()
+	}
+	
+	open func afterInit() {
+		add(createLayout)
+	}
+	
+	@UIViewBuilder
+	open func createLayout() -> SubviewsArrayConvertable {}
+	
+}
+
+open class LtViewController: UIViewController {
+	
+	open override func viewDidLoad() {
+		super.viewDidLoad()
+		view.add(createLayout)
+	}
+	
+	@UIViewBuilder
+	open func createLayout() -> SubviewsArrayConvertable {}
+	
+}
