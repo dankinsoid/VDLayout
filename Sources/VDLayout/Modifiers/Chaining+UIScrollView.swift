77c5797c9ee19b95cc5f43046d4e7febd337696f
dankinsoid <30962149+dankinsoid@users.noreply.github.com>
2022-11-17 20:29:25 +0400

    3.1.11

---
+++ Sources/VDLayout/Modifiers/Chaining+UIScrollView.swift
@@ -0,0 +1,35 @@
+import UIKit
+import VDChain
+
+public extension Chain where Base.Root: UIScrollView {
+    
+    func bounce(axis: NSLayoutConstraint.AxisSet = .both) -> Chain<DoChain<Base>> {
+        self.do {
+            $0.alwaysBounceVertical = axis.contains(.vertical)
+            $0.alwaysBounceHorizontal = axis.contains(.horizontal)
+        }
+    }
+    
+    func showsIndicators(_ axis: NSLayoutConstraint.AxisSet = .both) -> Chain<DoChain<Base>> {
+        self.do {
+            $0.showsVerticalScrollIndicator = axis.contains(.vertical)
+            $0.showsHorizontalScrollIndicator = axis.contains(.horizontal)
+        }
+    }
+    
+    func hideIndicators() -> Chain<DoChain<Base>> {
+        showsIndicators([])
+    }
+    
+    func paging(isEnabled: Bool = true) -> Chain<DoChain<Base>> {
+        self.do {
+            $0.isPagingEnabled = isEnabled
+        }
+    }
+    
+    func multitouch(isEnabled: Bool = true) -> Chain<DoChain<Base>> {
+        self.do {
+            $0.isMultipleTouchEnabled = isEnabled
+        }
+    }
+}
